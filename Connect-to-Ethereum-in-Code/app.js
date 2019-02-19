f59ebf267988cfb9d237972575bc93f10e3b664d
Jared Stauffer <30700558+jaredstauffer@users.noreply.github.com>
2019-02-19 11:54:36 -0700

    Create app.js

---
+++ Connect-to-Ethereum-in-Code/app.js
@@ -0,0 +1,35 @@
+/** 
+ * HOW TO Connect to the Ethereum Blockchain in Code
+ * 
+ * REQUIRES: NPM, Node.js, Web3.js, an Infura.io account (see https://infura.io)
+ *  
+ *           NPM and Node.js - https://www.npmjs.com/get-npm
+ * 
+ *           Web3.js install: $ npm install web3
+ *           https://www.npmjs.com/package/web3 
+ * 
+ *           Infura - create a free account, go to dashboard, by ENDPOINT select MAINNET, 
+ *           copy the end point URL, put it in the code below
+ * 
+ * TO RUN: node app.js
+ * 
+ * EXPECTED OUTPUT: a number (the latest block number, example: 7241437)
+ * 
+ * @ Part of the How to ... (in code) Series By Jared Stauffer 
+ * https://github.com/jaredstauffer
+ * https://twitter.com/jaredstauffer
+ */
+
+// import the Web3 library
+const Web3 = require('web3');
+
+// URL of the blockchain network
+var url = 'https://mainnet.infura.io/v3/61b8b2d78ce24663b24ca40e8f49c852';
+
+// create the connection
+var web3 = new Web3(url);
+
+// you have now created a connection to the Ethereum Blockchain Mainnet
+
+// to test the conenection we will get the latest block number
+web3.eth.getBlockNumber().then(console.log);
